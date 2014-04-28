f39c486c8478b5a37cc7a054b014325713d59658
Alex Nelson <alex@extreme-nelsons.net>
2014-04-28 11:29:57 -0400

    Moving SVN server commands to separate file.

--- src/net/extreme_nelsons/svnstats.clj
+++ src/net/extreme_nelsons/svnstats.clj
@@ -16,35 +16,8 @@
             [clojure.walk :refer [postwalk]]
             [incanter.core :as incant]
             [incanter.excel :as excel])
-  (:gen-class))
+  )
 
-(def svncommands {:svn "svn" :list "list" :log "log" :verbose "-v" :logxml "--xml" :co "co" :corev "-r"})
-(def bifDate (formatters :date))
-(def bifDateTime (formatters :date-time))
-
-(defn create-path
-  "Creates a path string"
-  [path]
-  (let [config (get-state :config) {:keys [repo base]} config]
-    (join "/" [repo base path])))
-
-(defn check-out-revision
-  "Checks out a specific revision of a line of code."
-  [path rev]
-  (let [{:keys [svn co corev]} svncommands]
-    (:out (sh svn co corev rev (create-path path)))))
-  
-(defn get-log-xml
-  "Gets the log data in xml format for the given path."
-  [path]
-  (let [{:keys [svn log verbose logxml]} svncommands]
-    (:out (sh svn log verbose logxml (create-path path))
-  )))
-
-(defn get-log-xml2
-  "fake input"
-  [p]
-  (slurp "xmllog.out"))
 
 (defn convert-xml-to-struct
   "Converts the project log xml to clojure structures"
